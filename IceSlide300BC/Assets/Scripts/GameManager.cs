efb713b09d3f01aa13682138f3ba560a6363a15c
Scott Robertson <scottrobertson97@gmail.com>
2018-01-24 09:59:29 -0500

    started game manager, player movenet broken

---
+++ IceSlide300BC/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    //array of things on the board
+    private int[,] board = new int[10,10];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //check if the player can move and handle what happens next
+    public bool PlayerMove (Vector2Int currentPos, Vector2Int futurePos) {
+        //check if the player is moving out of bounds
+        if(futurePos.x < 0 || futurePos.x >= 10 || futurePos.y < 0 || futurePos.y >= 10)
+            return false;
+        //makes sure that the player is moving onto a tile that is open
+        else if(board[futurePos.x, futurePos.y] == 0){
+            return true;
+        }
+        else
+            return false;
+    }
+}
--- IceSlide300BC/Assets/Scripts/Player.cs
+++ IceSlide300BC/Assets/Scripts/Player.cs
@@ -1,53 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum Direction
-{
-	North, 
-	South, 
-	East, 
-	West
-};
 public class Player : MonoBehaviour {
+    private GameManager gameManager;
+    private Vector2Int arrayPosition;
 
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3 (0.5f, 0.5f, 0.0f);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        arrayPosition = new Vector2Int((int)transform.position.x, (int)transform.position.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Move();
+	}
+
+	private void Move()
+	{
+        //movement direction
+        Vector2Int move = new Vector2Int();
+        
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
 		{
-			Move (Direction.West);
+            move += Vector2Int.left;
 		}
 		if (Input.GetKeyDown (KeyCode.RightArrow)) 
 		{
-			Move (Direction.East);
+			move += Vector2Int.right;
 		}
 		if (Input.GetKeyDown (KeyCode.UpArrow)) 
 		{
-			Move (Direction.North);
+			move += Vector2Int.up;
 		}
 		if (Input.GetKeyDown (KeyCode.DownArrow)) 
 		{
-			Move (Direction.South);
+			move += Vector2Int.down;
 		}
-	}
 
-	private void Move(Direction direction)
-	{
-		Vector3 move = new Vector3 ();
-		if (direction == Direction.North) {
-			move = new Vector3 (0, 1, 0);
-		} else if (direction == Direction.South) {
-			move = new Vector3 (0,-1,0);
-		} else if (direction == Direction.East) {
-			move = new Vector3 (1, 0, 0);
-		} else if (direction == Direction.West) {
-			move = new Vector3 (-1, 0, 0);
-		}
-		transform.position += move;
+        if(move == Vector2Int.zero)
+            return;
+
+        bool canMove = gameManager.PlayerMove(arrayPosition, arrayPosition + move);
+        if(canMove){
+            arrayPosition += move;
+            transform.position += new Vector3Int(arrayPosition.x, arrayPosition.y, 0);
+        }
 		/*
 		Implement LERP to new position
 		*/
