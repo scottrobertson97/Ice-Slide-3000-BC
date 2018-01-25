99ff89a41441717aa7e324844fdd3a1dfb686a3f
Scott Robertson <smr7178@rit.edu>
2018-01-25 00:18:06 -0500

    did lerp player movement and structured game manager

--- IceSlide300BC/Assets/Scripts/GameManager.cs
+++ IceSlide300BC/Assets/Scripts/GameManager.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+	//wall
+	public GameObject wallPrefab;
+	public GameObject acorn;
+
+	private enum Board : int {Floor = 0, Wall = 1, Acorn = 2};
     //array of things on the board
-    private int[,] board = new int[10,10];
+	private Board[,] board = new Board[10,10];
 
 	// Use this for initialization
 	void Start () {
-		
+		//make 5,5 a wall
+		board [5, 5] = Board.Wall;
+		createWalls ();
 	}
 	
 	// Update is called once per frame
@@ -16,16 +23,47 @@ public class GameManager : MonoBehaviour {
 		
 	}
 
-    //check if the player can move and handle what happens next
-    public bool PlayerMove (Vector2Int currentPos, Vector2Int futurePos) {
+	/// <summary>
+	/// Creates the sprites for the walls
+	/// </summary>
+	void createWalls(){
+		for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10; y++) {
+				if (board [x, y] == Board.Wall) {
+					Instantiate (wallPrefab, new Vector3 (x, y, 0), Quaternion.identity);
+				}
+			}
+		}
+	}
+
+    /// <summary>
+	/// check if the player can move and handle what happens next
+    /// </summary>
+    /// <returns><c>true</c>, if the player can move, <c>false</c> otherwise.</returns>
+    /// <param name="currentPos">Current position of the player.</param>
+    /// <param name="futurePos">Future position of the player.</param>
+    public bool CanPlayerMove (Vector2Int currentPos, Vector2Int futurePos) {
         //check if the player is moving out of bounds
-        if(futurePos.x < 0 || futurePos.x >= 10 || futurePos.y < 0 || futurePos.y >= 10)
-            return false;
+		if (futurePos.x < 0 || futurePos.x >= 10 || futurePos.y < 0 || futurePos.y >= 10) {
+			return false;
+		}
         //makes sure that the player is moving onto a tile that is open
-        else if(board[futurePos.x, futurePos.y] == 0){
+		else if(board[futurePos.x, futurePos.y] == Board.Floor){
             return true;
         }
+		else if(board[futurePos.x, futurePos.y] == Board.Acorn){
+			return MoveAcorn (futurePos - currentPos);
+		}
         else
             return false;
     }
+
+	/// <summary>
+	/// Moves the acorn as far as it can in the direction, untill it hits a wall or falls off
+	/// </summary>
+	/// <returns><c>true</c>, if acorn was moved, <c>false</c> otherwise.</returns>
+	/// <param name="direction">Direction.</param>
+	private bool MoveAcorn(Vector2Int direction){
+		return false;
+	}
 }
--- IceSlide300BC/Assets/Scripts/Player.cs
+++ IceSlide300BC/Assets/Scripts/Player.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     private GameManager gameManager;
     private Vector2Int arrayPosition;
+	private Vector2Int futurePosisiton;
+	private static float MOVE_TIME = 0.25f;
+	private float moveTimer;
+	private bool isMoving;
 
 	// Use this for initialization
 	void Start () {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        arrayPosition = new Vector2Int((int)transform.position.x, (int)transform.position.y);
+		arrayPosition = new Vector2Int((int)transform.position.x, (int)transform.position.y);
+		moveTimer = 0;
+		isMoving = false;
 	}
 	
 	// Update is called once per frame
@@ -16,11 +22,22 @@ public class Player : MonoBehaviour {
 		Move();
 	}
 
+	/// <summary>
+	/// Handles player movement and input
+	/// </summary>
 	private void Move()
 	{
+		//what to so if the player is currently moving
+		//this returns at the end, so if the player is moving then there is not input
+		if (isMoving) {
+			LERP ();
+			return;
+		}
+
         //movement direction
-        Vector2Int move = new Vector2Int();
+		Vector2Int move = new Vector2Int();
         
+		//get input and the direction vector
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
 		{
             move += Vector2Int.left;
@@ -38,16 +55,45 @@ public class Player : MonoBehaviour {
 			move += Vector2Int.down;
 		}
 
-        if(move == Vector2Int.zero)
+		//if it is zero, no input was given, so return
+		if(move == Vector2Int.zero)
             return;
 
-        bool canMove = gameManager.PlayerMove(arrayPosition, arrayPosition + move);
+		//see if the player can move into that spot
+        bool canMove = gameManager.CanPlayerMove(arrayPosition, arrayPosition + move);
+		//if they can
         if(canMove){
-            arrayPosition += move;
-            transform.position += new Vector3Int(arrayPosition.x, arrayPosition.y, 0);
+			//set moving
+			isMoving = true;
+			//set future position
+			futurePosisiton = arrayPosition + move;
         }
 		/*
 		Implement LERP to new position
 		*/
+	}
+
+	private void LERP(){
+		//get the direction vector
+		Vector2 currentLerp = futurePosisiton-arrayPosition;
+		//get the percent of that
+		currentLerp *= (moveTimer / MOVE_TIME);
+		//add the current pos to get what the position should be right now
+		currentLerp += arrayPosition;
+		//set the position
+		transform.position = new Vector3 (currentLerp.x, currentLerp.y, 0);
+		//increment timer
+		moveTimer += Time.deltaTime;
+
+		//if the timer is up
+		if (moveTimer > MOVE_TIME) {
+			//reset
+			moveTimer = 0;
+			isMoving = false;
+			//set the arrayPos
+			arrayPosition = futurePosisiton;
+			//set the transform
+			transform.position = new Vector3(arrayPosition.x, arrayPosition.y, 0);
+		}
 	}
 }
