ad4c4317321de5098562d957d1dc234cb8bb8dae
JACKSONSPC\jchar_000 <jmc2817@g.rit.edu>
2018-01-29 01:29:10 -0500

    added start menu scene, ui manager object. created ui manager script and added stubs to address different buttons, canvas needs (future)

---
+++ IceSlide300BC/Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    // start button on main menu
+    public void LoadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("main");
+    }
+
+    // load main menu, for use with going back from other screens
+    public void LoadMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("start");
+    }
+
+    // display high scores
+    public void LoadHighScores()
+    {
+        // should we just use the main menu scene for these and show/hide different canvases? same with level select
+    }
+
+    // display level select screen
+    public void LoadLevelSelect()
+    {
+
+    }
+
+    // display game end screen
+    public void GameOver()
+    {
+
+    }
+
+    // exit game
+    public void ExitGame()
+    {
+        // will work once the game is built to exe
+        Application.Quit();
+    }
+}
